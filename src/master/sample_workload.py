e4de4a3031ece81daef7a751978b120043a25df4
Ashutosh Mondal <your.email@example.com>
2025-09-12 23:45:25 +0530

    main -v3

---
+++ src/master/sample_workload.py
@@ -0,0 +1,21 @@
+import ray
+import time
+
+@ray.remote
+def sample_task(x):
+    time.sleep(1) # Simulate some work
+    return x * x
+
+if __name__ == "__main__":
+    ray.init(address="auto") # Connect to the existing Ray cluster
+
+    # Submit a series of tasks
+    results = []
+    for i in range(100):
+        results.append(sample_task.remote(i))
+
+    # Get and print the results
+    output = ray.get(results)
+    print("Sample workload results:", output)
+
+    # You can also check the Ray dashboard (usually at http://<master-ip>:8265) to see load distribution.
